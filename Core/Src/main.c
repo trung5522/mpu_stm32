e36f71b3a33461f09a72ce99abfa01ced76a9637
trung5522 <trung.pham0550@hcmut.edu.vn>
2023-03-06 15:59:01 +0700

    lan2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+/**
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
